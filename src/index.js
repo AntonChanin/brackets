3a0d7d00ead18c8f6d4ffaa8142b830526f4b90c
antonvh <antonhaninvl@gmail.com>
2019-10-13 17:02:33 +0300

    feat: successfull all ex if ([)]

--- src/index.js
+++ src/index.js
@@ -1,39 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
     let outPut = true;
-    let BracketBool = true;
-    let parenthesis = 0;
-        for (var i = 0; i < str.length; i++) {
-            var symetricParenthesis = 0;
-            if (BracketBool != false) {
-                for (var j = 0; j < bracketsConfig.length; j++) {
-                    if (parenthesis === -1) {
-                        BracketBool = false;
-                    }
-                    if (parenthesis != -1 ) {
-                        if ( i === j || str[i] == bracketsConfig[j][1]) {
-                            if (str[i] === bracketsConfig[j][0]) {
-                                parenthesis += 1;
-                            } else {
-                                parenthesis -= 1;
-                            }
-                        }
-                       if ( i === j || str[i] == bracketsConfig[j][1]) {
-                            if (bracketsConfig[j][1] === bracketsConfig[j][0]) {
-                                if (str[i] === bracketsConfig[j][0]) {
-                                    parenthesis += 1;
-                                }
-                                if (str[i] === bracketsConfig[j][1]) {
-                                    parenthesis -= 1;
-                                }
-                            }
-                        }
-                    } else {
-                        BracketBool = false;
-                        outPut = false;
-                    }
-                }
-           }
-        }
+    count = 0;
+	k = 0;
+    for (let i = 0; i < str.length; i++) {
+		if (str[i] == bracketsConfig[k][0]) {
+			count++;
+		};
+		if (str[i] == bracketsConfig[k][1]) {
+			count--;
+		}
+		if (count < 0) {
+			outPut = false;
+			break;
+		}  if (i == str.length){
+			i = 0;
+			k++; 
+		}
+	}
     return outPut;
 }
