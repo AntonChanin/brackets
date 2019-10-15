0da52532193b2c0931f5c0ef92208a646cef5e5c
antonvh <antonhaninvl@gmail.com>
2019-10-15 13:23:08 +0300

    feat 16/20 tests successfull other failed becouse does not support nesting

--- src/index.js
+++ src/index.js
@@ -1,22 +1,54 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
     let outPut = true;
-    count = 0;
-	k = 0;
-    for (let i = 0; i < str.length; i++) {
-		if (str[i] == bracketsConfig[k][0]) {
-			count++;
-		};
-		if (str[i] == bracketsConfig[k][1]) {
-			count--;
-		}
-		if (count < 0) {
-			outPut = false;
-			break;
-		}  if (i == str.length){
-			i = 0;
-			k++; 
-		}
+    let count = 0;
+    let openBrackets = [];
+    let closeBrackets = [];
+    
+	for (let i = 0; i < bracketsConfig.length; i++) {
+		openBrackets.push(bracketsConfig[i][0]);
 	}
+	for (let i = 0; i < bracketsConfig.length; i++) {
+		closeBrackets.push(bracketsConfig[i][1]);
+	}
+	if (str.length % 2 != 0) {
+		outPut = false;
+	} else {
+		if (openBrackets.includes(str[0])) {
+		count++;
+			for (let j = 1; j <= str.length-1; j++) {
+				if (openBrackets.includes(str[j])) {
+					count++;
+		    		/*if (str[j+1] != undefined && str[j+1] != bracketsConfig[closeBrackets.indexOf(str[j+1])][1] ) {
+		    			outPut = false;
+		    			break;
+		    		}*/	
+				} else {
+					if (closeBrackets.includes(str[j])) {
+						if (count==0) {
+							count--;
+							if (count < 0) {
+								outPut = false;
+								break;
+							}
+						}							
+						if (str[j] == bracketsConfig[closeBrackets.indexOf(str[j], 0)][1] && str[j-1] == bracketsConfig[closeBrackets.indexOf(str[j], 0)][0]) {
+							count--;
+					    } 
+					    if (count < 0) {
+							outPut = false;
+							break;
+						}
+					}	
+				} if (count < 0) {
+					outPut = false;
+					break;
+				}	
+			}	
+		} else { 
+		outPut = false;
+		}		
+	}
+	
     return outPut;
 }
